438d4579ebc46f84a00fd5d59670a0d5bd453752
oOBobbyOo <944831575@qq.com>
2025-12-23 11:23:17 +0800

    refactor: change package scripts and tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: 'src/extension.ts',
+  entry: [
+    'src/extension.ts',
+  ],
+  format: ['cjs'],
   outDir: 'out',
-  format: 'cjs',
-  splitting: false,
-  sourcemap: false,
   shims: false,
-  declaration: true,
-  external: ['vscode'],
+  dts: false,
+  external: [
+    'vscode',
+  ],
 })
